53f44dd9583faf27b00c99ebe7a2c4cd41e3401f
Yomaira Escano <yjescano@gmail.com>
2018-10-21 04:35:13 +0000

    automatically backed up by learn

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,9 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-# Dotenv::Railtie.load
+Dotenv::Railtie.load
+
+HOSTNAME = ENV['HOSTNAME']
 
 module ApisAndFaradayReading
   class Application < Rails::Application
